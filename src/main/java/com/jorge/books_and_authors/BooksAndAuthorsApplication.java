c4a3fe1e52d23ce0e65998f95f0adc51ce0f456d
KainSlow <jorge_alfonso3@hotmail.com>
2025-05-06 20:31:53 -0400

    Init project

---
+++ src/main/java/com/jorge/books_and_authors/BooksAndAuthorsApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.books_and_authors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAndAuthorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAndAuthorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorge/books_and_authors/BooksAndAuthorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.books_and_authors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAndAuthorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
